77113509cc314b2d91570223cd4d4757e602b999
Anas Almutary <00063q@gmail.com>
2026-01-03 19:44:08 +0300

    update globals.css

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,4 +11,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
