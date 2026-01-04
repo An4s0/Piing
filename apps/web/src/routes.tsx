534ec71113745759c570bf6c3f4c07043530a2df
Anas Almutary <00063q@gmail.com>
2026-01-04 18:07:18 +0300

    update web structure

--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -3,15 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import { Loading } from "@/components/loading";
 
 export default function AppRoutes() {
-  const NotFoundPage = React.lazy(() => import("./pages/404/page"));
-  const HomePage = React.lazy(() => import("./pages/home/page"));
-  const SignUpPage = React.lazy(() => import("./pages/auth/signup"));
-  const SignInPage = React.lazy(() => import("./pages/auth/signin"));
+  const NotFoundPage = React.lazy(
+    () => import("./pages/not-found/NotFoundPage")
+  );
+  const HomePage = React.lazy(() => import("./pages/home/HomePage"));
+  const RemindersPage = React.lazy(
+    () => import("./pages/reminders/RemindersPage")
+  );
+  const NewReminderPage = React.lazy(
+    () => import("./pages/reminders/NewReminderPage")
+  );
+  const SignUpPage = React.lazy(() => import("./pages/auth/SignUpPage"));
+  const SignInPage = React.lazy(() => import("./pages/auth/SignInPage"));
 
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/reminders" element={<RemindersPage />} />
+        <Route path="/reminders/new" element={<NewReminderPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
 
