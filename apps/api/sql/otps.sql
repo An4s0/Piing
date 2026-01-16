abe3fb9b5b50424f2cbfda6dfa21146159b12ec9
Anas Almutary <00063q@gmail.com>
2026-01-16 21:36:05 +0300

    update database tables

---
+++ apps/api/sql/otps.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+    IF NOT EXISTS otps (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+        code VARCHAR(6) NOT NULL,
+        expires_at TIMESTAMPTZ NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    );
--- apps/api/sql/users.sql
+++ apps/api/sql/users.sql
@@ -1,8 +1,10 @@
 CREATE TABLE
-  users (
+  IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(100),
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    password_hash TEXT NOT NULL,
+    is_verified BOOLEAN NOT NULL DEFAULT FALSE,
+    verification_expires_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
   );
