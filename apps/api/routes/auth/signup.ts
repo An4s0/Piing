e8a0801bfda45c77194dad4b834dec43b145aab6
Anas Almutary <00063q@gmail.com>
2026-01-18 06:14:45 +0300

    add signup route

---
+++ apps/api/routes/auth/signup.ts
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import bcrypt from "bcryptjs";
+import { signupSchema } from "@piing/validation";
+import { usersService, otpsService } from "@/services";
+import { sendOtp } from "@/utils/send-otp";
+import type { ApiResponse } from "@/types";
+import type { IUser } from "@piing/types";
+
+const router: Router = Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const parseResult = signupSchema.safeParse(req.body);
+    if (!parseResult.success) {
+      throw parseResult.error;
+    }
+
+    const { name, email, password } = req.body;
+
+    const existingUser = await usersService.findOne({
+      email: email.toLowerCase(),
+    });
+    if (existingUser) {
+      throw new Error("EMAIL_ALREADY_EXISTS");
+    }
+
+    const user = await usersService.create({
+      name,
+      email,
+      password_hash: await bcrypt.hash(password, 10),
+    });
+    if (!user) {
+      throw new Error("USER_CREATION_FAILED");
+    }
+
+    const otpCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const otp = await otpsService.create({
+      user_id: user.id,
+      code: otpCode,
+      expires_at: new Date(Date.now() + 10 * 60 * 1000), // 10 Minutes
+    });
+    if (!otp) {
+      throw new Error("OTP_CREATION_FAILED");
+    }
+
+    await sendOtp({
+      to: email,
+      otp: otpCode,
+    });
+
+    const { password_hash, ...safeUser } = user;
+    res.json({
+      success: true,
+      data: { user: safeUser },
+      error: null,
+    } satisfies ApiResponse<{ user: IUser }>);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
